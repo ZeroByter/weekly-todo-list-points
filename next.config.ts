ad3cdfe7a429469cdb1e113a8c540dec83d4a6c1
ZeroByter <romgamesrom@gmail.com>
2025-04-15 14:11:57 +0300

    added back export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: isProd ? "/weekly-todo-list-points" : "",
   assetPrefix: isProd ? "/weekly-todo-list-points/" : "",
   trailingSlash: true, // required for GitHub Pages
