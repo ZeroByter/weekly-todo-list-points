b948e43581cca59e676284fa7c3f08a85af0d347
ZeroByter <romgamesrom@gmail.com>
2025-04-15 14:10:32 +0300

    messing with configs

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/weekly-todo-list-points",
-  assetPrefix: "https://zerobyter.github.io/weekly-todo-list-points",
+  basePath: isProd ? "/weekly-todo-list-points" : "",
+  assetPrefix: isProd ? "/weekly-todo-list-points/" : "",
+  trailingSlash: true, // required for GitHub Pages
 };
 
 export default nextConfig;
