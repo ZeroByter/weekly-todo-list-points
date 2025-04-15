e6c3379bc551d1aefde3bcf64a37c20af097accd
ZeroByter <romgamesrom@gmail.com>
2025-04-15 12:16:20 +0300

    created main task row added gh-pages

--- src/app/components/pages/tasks/task.tsx
+++ src/app/components/pages/tasks/task.tsx
@@ -1,3 +1,5 @@
+import { useMainData } from "@/app/contexts/mainData";
+import { dateToWeekIndex } from "@/app/utils";
 import TaskType from "@/types/task";
 import { FC } from "react";
 
@@ -6,11 +8,42 @@ type Props = {
 };
 
 const Task: FC<Props> = ({ task }) => {
+  const { tasksPerWeek, setTasksPerWeek, currentWeekDate } = useMainData();
+
+  const weekIndex = dateToWeekIndex(currentWeekDate);
+
+  const getIsChecked = () => {
+    if (!(weekIndex in tasksPerWeek)) {
+      return false;
+    }
+
+    return tasksPerWeek[weekIndex].includes(task.id);
+  };
+
+  const isChecked = getIsChecked();
+
+  const handleCheckedChange = () => {
+    if (!(weekIndex in tasksPerWeek)) {
+      setTasksPerWeek({ ...tasksPerWeek, [weekIndex]: [] });
+    }
+
+    setTasksPerWeek({
+      ...tasksPerWeek,
+      [weekIndex]: isChecked
+        ? tasksPerWeek[weekIndex].filter((loopTaskId) => loopTaskId != task.id)
+        : [...tasksPerWeek[weekIndex], task.id],
+    });
+  };
+
   return (
     <tr>
       <td>{task.description}</td>
       <td>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckedChange}
+        />
       </td>
     </tr>
   );
